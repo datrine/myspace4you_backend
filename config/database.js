dcd1114bfa55bea7ea312c395d69a34d08bd2d70
datrine <trinitietp@gmail.com>
2021-04-14 05:46:15 +0100

    testing something

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       settings: {
         client: 'mysql',
         host: env('DATABASE_HOST', 'eel.whogohost.com'),
-        port: env.int('DATABASE_PORT', 3306),
+        port: env.int('DAListingListingTABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'stylezns_roomsdb'),
         username: env('DATABASE_USERNAME', 'stylezns_datrine'),
         password: env('DATABASE_PASSWORD', 'TeMi4ToPe'),
