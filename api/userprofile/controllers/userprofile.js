bf241cc7ad6d59d9c366a137fddd677ee10e23e0
datrine <trinitietp@gmail.com>
2021-04-22 07:51:04 +0100

    first step

---
+++ api/userprofile/controllers/userprofile.js
@@ -0,0 +1,191 @@
+'use strict';
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+/**
+ * @type {import('strapi').Strapi strapi
+ * }
+ */
+
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+const formatError = error => [
+    { messages: [{ id: error.id, message: error.message, field: error.field }] },
+];
+
+module.exports = {
+
+    //controller for /userprofiles =>create
+    async create(ctx) {
+        let { body: bodyReq } = ctx.request;
+        if (bodyReq) {
+            if (bodyReq.gggg) {
+                return ctx.badRequest(
+                    null,
+                    formatError({
+                        id: 'Auth.form.error.address',
+                        message: 'Address field cannot be empty.',
+                        field: ['address'],
+                    })
+                );
+            }
+            let userPermissionController = strapi.plugins['users-permissions'].controllers.user;
+            await userPermissionController.create(ctx);
+            let res = ctx.response
+            if (res.status >= 200 && res.status < 300) {
+                let userSaved = res.body
+                if (userSaved) {
+                    await strapi.query('userprofile').model.query(
+                        /**
+                         * @param {import("knex").QueryBuilder} knex
+                         */
+                        async function (knex) {
+                            //console.log("1ajfasgjasghvhasvhs")
+                            let { password, username, ...rest } = bodyReq
+                            let profiles = await knex.insert({ userId: userSaved.id, ...rest })
+                            //console.log("2ajfasgjasghvhasvhs")
+                            //console.log(profiles)
+                            if (profiles.length > 0) {
+                                //console.log("3ajfasgjasghvhasvhs")
+                                const result = await strapi.query('userprofile').model.
+                                    query(function (qb) {
+                                        console.log("4ajfasgjasghvhasvhs")
+                                        qb.where({ userId: userSaved.id });
+                                    }).fetchAll();
+
+                                const fields = result && result.toJSON();
+                                let { id, rest } = fields[0];
+                                userSaved = { ...userSaved, ...rest, profileId: id }
+                                ///console.log(userSaved)
+                                res.body = userSaved
+                                ctx.response._body.user = userSaved
+                            }
+
+                        });
+                    console.log("jtrsesrwawe")
+                }
+            }
+            else {
+                console.log("unable to save")
+            }
+        } else {
+            return ctx.badRequest(
+                null,
+                formatError({
+                    id: 'Auth.form.error.multiple_fields',
+                    message: 'User fields are empty.',
+                    field: ['username', 'password'],
+                })
+            );
+        }
+    },
+    // controller for /api/userprofiles/login
+    async login(ctx) {
+        if (ctx.request) {
+            // console.log(Object.keys(ctx))
+            let { identifier, password } = ctx.request.body;
+            //console.log(ctx.request.body)
+            //console.log(Object.keys(strapi.plugins))
+            //console.log(strapi.models)
+            let authLoginPermissionController = strapi.plugins['users-permissions'].controllers.auth;
+
+            await authLoginPermissionController.callback(ctx);
+            let { jwt, user } = ctx.response._body;
+            if (user) {
+                const result = await strapi.query('userprofile').model.query(function (qb) {
+                    qb.where({ userId: user.id });
+                }).fetchAll();
+
+                const fields = result && result.toJSON();
+                if (fields.length > 0) {
+                    let profileData = fields[0]
+                    let { id: profileId, ...restOfProfile } = profileData
+                    let { jwt, user } = ctx.response._body
+                    console.log(jwt)
+                    user = { ...user, ...restOfProfile, profileId }
+                    console.log(user.username)
+                    ctx.response._body = {
+                        jwt, user
+                    }
+                } else {
+                    await strapi.query('userprofile').model.query(
+                        /**
+                         * @param {import("knex").QueryBuilder} knex
+                         */
+                        async function (knex) {
+                            let { id: userId, email, ...restOfUser } = user
+                            let profiles = await knex.insert({ userId, email })
+                            if (profiles.length > 0) {
+                                //console.log("3ajfasgjasghvhasvhs")
+                                const result = await strapi.query('userprofile').model.
+                                    query(function (qb) {
+                                        qb.where({ userId });
+                                    }).fetchAll();
+
+                                const fields = result && result.toJSON();
+                                let { id: profileId, ...rest } = fields[0];
+                                let userSaved = { ...user, ...rest, profileId }
+                                console.log(restOfUser)
+                                res.body = userSaved
+                                ctx.response._body.user = userSaved
+                            }
+
+                        });
+                }
+            }
+            else {
+                console.log("ytygygugyu")
+            }
+        } else {
+            const result = await strapi.query('userprofile').model.fetchAll();
+            const fields = result && result.toJSON();
+            console.log(fields);
+            return fields;
+        }
+    },
+
+    // controller for GET /api/userprofiles/id
+    /**
+     * 
+     * @param {koa} ctx 
+     * @returns 
+     */
+    async findOne(ctx) {
+        if (ctx.request) {
+            // console.log(Object.keys(ctx))
+            let { url: initialUrl } = ctx.request;
+            let userId = initialUrl.split("/")[2]    //e.g is in /userprofiles/1 => ["","userprofile",1]
+            console.log(userId)
+
+            const result = await strapi.query('userprofile').model.query(function (qb) {
+                qb.where({ id: userId });
+            }).fetchAll();
+
+            const fields = result && result.toJSON();
+            console.log(fields)
+            if (fields.length > 0) {
+                let profileData = fields[0]
+                let { id: profileId, ...restOfProfile } = profileData
+
+                let userprofile = { ...restOfProfile, profileId }
+
+                let userResult = await strapi.plugins["users-permissions"].models["user"].query(function (qb) {
+                    qb.where({ id: userprofile.userId });
+                }).fetchAll();
+                const userFields = userResult && userResult.toJSON();
+                let { id: userId, restOfUser } = userFields[0]
+                userprofile = { ...restOfUser, userId, ...userprofile }
+                ctx.response._body = userprofile
+                ctx.response.status = 200
+            } else {
+            }
+
+        } else {
+            const result = await strapi.query('userprofile').model.fetchAll();
+            const fields = result && result.toJSON();
+            console.log(fields);
+            return fields;
+        }
+    },
+};
